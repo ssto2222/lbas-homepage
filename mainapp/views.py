8482b5b53e684ce798dd0617e702deb089291146
tuttofare <tech2metail@gmail.com>
2024-04-18 15:34:39 +0900

    first commit

---
+++ mainapp/views.py
@@ -0,0 +1,8 @@
+from django.views.generic import View
+from django.shortcuts import render
+
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "mainapp/index.html")
+
