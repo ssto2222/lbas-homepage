5d6ebcf3c954342b6efd8d85e946e74607672ec0
tuttofare <tech2metail@gmail.com>
2024-04-19 18:02:31 +0900

    user manager

--- mainapp/management/commands/superuser.py
+++ mainapp/management/commands/superuser.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand
+from mainapp.models.account_models import UserManager
 #from django.contrib.auth import get_user_model
 from django.conf import settings
 from mainapp.models.user_models import User
@@ -8,9 +9,10 @@ from mainapp.models.user_models import User
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        if not User.objects.filter(username=settings.SUPERUSER_NAME).exists():
-            User.objects.create_superuser(
+        if not User.objects.filter(email=settings.SUPERUSER_NAME).exists():
+            UserManager.create_superuser(
                 email=settings.SUPERUSER_EMAIL,
                 password=settings.SUPERUSER_PASSWORD
             )
+            
             print("スーパーユーザー作成")
