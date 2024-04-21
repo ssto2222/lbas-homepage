3258c7a32d49e72c47b1e38b98622ee5bed661d5
tuttofare <tech2metail@gmail.com>
2024-04-21 18:30:24 +0900

    orders

--- orders/models.py
+++ orders/models.py
@@ -3,11 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 from store.models import Product
-import uuid
+#import uuid
 
 
 class Order(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order_user')
     #deadline=models.DateTimeField(default=timezone.now)
     full_name = models.CharField(max_length=50)
