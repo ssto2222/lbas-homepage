8dec5ae397596644e86e7e8361bcaae44249c213
tuttofare <tech2metail@gmail.com>
2024-04-21 18:18:20 +0900

    orders

--- orders/models.py
+++ orders/models.py
@@ -7,7 +7,7 @@ import uuid
 
 
 class Order(models.Model):
-    #id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='order_user')
     #deadline=models.DateTimeField(default=timezone.now)
     full_name = models.CharField(max_length=50)
